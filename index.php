854e1e2b9d9c82f93cad2f24c037285345c10729
Tim Andreas <tim.andreas@emdion.eu>
2022-12-14 14:22:55 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+
+        <title>SG-OS</title>
+        
+        <!--MetaTags -> Immer Benutzen-->
+        <meta content="IE=edge" http-equiv="X-UA-Compatible" />
+        <meta name="viewport" content="width=device-width,initial-scale=1"  />
+  
+        <!--MetaTags-->
+        <link rel="stylesheet" type="text/css" href="./css/styles.css" />
+        <!--<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> -->
+    </head>
+    <body id="main-background">
+
+        <header class="header">            
+        Header new stuff
+        </header>      
+        <div class="container">
+            <div class="row border">
+                <div class="left-content border">
+                    <p class="typewriter"> First Row Left Content </p>
+                </div>  
+                <div class="right-content border">
+                    First Riw Right Content
+                </div>
+            </div>          
+        </div>                   
+    </body>    
+</html>
